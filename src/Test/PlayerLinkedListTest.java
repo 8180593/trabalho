bbb96f13e51cc5655394e65984ef0d5d8d9f8eab
Duarte Pires <orlandoduartepires@gmail.com>
2023-01-24 10:57:06 +0000

    Junit ja funciona, testes não rodados

--- src/Test/PlayerLinkedListTest.java
+++ src/Test/PlayerLinkedListTest.java
@@ -3,16 +3,21 @@ package Test;
 import Player.Equipas;
 import Player.Player;
 import Player.PlayerLinkedList;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 class PlayerLinkedListTest {
+    @Test
     void addPlayerTest(){
         Player player = new Player("Rui", Equipas.Sparks);
         PlayerLinkedList playerLinkedList = new PlayerLinkedList();
         playerLinkedList.addPlayer(player);
         assertEquals(playerLinkedList.getNext().getPlayer().getName(), "Rui");
     }
-
-    void addPlayerTest(){
+    @Test
+    void addPlayerTest1(){
         Player player = new Player("Rui", Equipas.Sparks);
         PlayerLinkedList playerLinkedList = new PlayerLinkedList();
         playerLinkedList.removePlayer(player);
