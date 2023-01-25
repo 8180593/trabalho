3f5b8aba575352a89eaf159272b18f640877d190
Duarte Pires <orlandoduartepires@gmail.com>
2023-01-25 14:11:36 +0000

    GUI adicioanr Jogador

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -10,20 +10,31 @@ import java.awt.event.ActionListener;
 
 public class GUI implements ActionListener {
     private LinkedList<Player> players = new LinkedList<>();
+    private JFrame frame = new JFrame("JFrame Example");
+    private JButton button = new JButton("Adicionar Jogador");
+    private JLabel label = new JLabel(" Adicionar Jogadores");
+    private JLabel jogador = new JLabel("Digite o nome do Jogador:");
+    private JTextField fieldName = new JTextField();
+    private Checkbox Sparks = new Checkbox("Sparks");
+    private Checkbox Giants = new Checkbox("Giants");
     public GUI(){
-        JFrame frame = new JFrame("JFrame Example");
-
-        JButton button = new JButton("Adicionar Jogador");
-        button.setPreferredSize(new Dimension(175, 75));
+        button.setBounds(250, 150,100,20);
         button.addActionListener(this);
+        label.setBounds(100,100, 100,20);
+        jogador.setBounds(100,100, 100,20);
+        fieldName.setBounds(150, 100, 100, 10);
 
-        JLabel label = new JLabel(" Adicionar Jogadores");
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(75, 75, 150, 300));
         panel.setLayout(new GridLayout(0, 1));
         panel.add(label);
+        panel.add(jogador);
+        panel.add(fieldName);
+        panel.add(Sparks);
+        panel.add(Giants);
         panel.add(button);
 
+        frame.setSize(500, 500);
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("8210311 Daniela Moreira // 8210367 Orlando Pires");
@@ -32,8 +43,21 @@ public class GUI implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        players.add(new Player("Daniela", Equipas.Giants));
-        System.out.println(players.getFront().getElement().getName());
+        if(e.getSource() == button)
+            addPlayer();;
+    }
+    public void addPlayer(){
+        if(fieldName.getText().isEmpty()){
+            JOptionPane.showMessageDialog(null, "Digite o nome do jogador");
+        }
+        if(!Sparks.getState() && Giants.getState())
+            players.add(new Player(fieldName.getText(), Equipas.Sparks));
+        else if(Sparks.getState() && !Giants.getState())
+            players.add(new Player(fieldName.getText(), Equipas.Giants));
+        else if(Sparks.getState() && Giants.getState())
+            JOptionPane.showMessageDialog(null, "So pode selecionar uma equipa");
+        else
+            JOptionPane.showMessageDialog(null, "Selecione uma equipa");
     }
     public static void main(String[] args) {
         new GUI();
