d3aed6f4f9085cbe166a910ac46400ef999229c7
Daniela Moreira <8210311@estg.ipp.pt>
2023-01-26 20:50:16 +0000

    Update

--- src/Player/Equipa.java
+++ src/Player/Equipa.java
@@ -3,7 +3,7 @@ package Player;
 import ClassImplementation.LinkedList;
 
 /**
- * @author 8210311 Daniele Moreira
+ * @author 8210311 Daniela Moreira
  * @author 8210367 Orlando Pires
  */
 public class Equipa {
--- src/Player/Equipas.java
+++ src/Player/Equipas.java
@@ -1,6 +1,6 @@
 package Player;
 /**
- * @author 8210311 Daniele Moreira
+ * @author 8210311 Daniela Moreira
  * @author 8210367 Orlando Pires
  */
 public enum Equipas {
